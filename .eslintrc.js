207a5a63b5c70ac59fa25be00e18edfb4a95e1e4
Guillaume <guillaume.trb@hotmail.com>
2021-06-15 11:28:00 +0200

    fix: eslint config forgotten

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true,
+    browser: true
+  },
+
+  plugins: ['prettier'],
+
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+
+  extends: [
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    'prettier/vue',
+    'plugin:prettier/recommended'
+  ],
+  
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
+}
