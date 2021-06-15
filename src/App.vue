eb26c18599b53c0316fe39c832152a4c2904415f
JyToSTT <jean.tostivint@gmail.com>
2021-06-15 15:53:49 +0200

    > Cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,8 @@
 <template>
-  <!--  <router-view />-->
-  <section>
-    <article>
-      <Button text="Hello world !" interface="fill"></Button>
-      <Button text="Hello world !" interface="fill" disabled></Button>
-      <Button text="Hello world !" interface="outline"></Button>
-      <Button text="Hello world !" interface="outline" disabled></Button>
-    </article>
-
-    <article>
-      <Register
-        text="Register"
-        href="https://www.jean-tostivint.dev/"
-      ></Register>
-      <br />
-      <Register
-        text="Register"
-        icon-left="arrow-left"
-        href="https://www.jean-tostivint.dev/"
-      ></Register>
-      <br />
-
-      <Register
-        text="Register"
-        icon-right="external-link"
-        href="https://www.jean-tostivint.dev/"
-      ></Register>
-    </article>
-  </section>
+  <router-view />
 </template>
 
 <style lang="scss" src="./assets/scss/styles.scss"></style>
 <script>
-import Button from "@/components/buttons/Button";
-import Register from "@/components/buttons/Register";
-
-export default {
-  components: { Button, Register },
-};
+export default {};
 </script>
