48dec76b616db51708b53527c7119d7595ac970d
Guillaume <guillaume.trb@hotmail.com>
2021-06-15 11:32:07 +0200

    feat: removes default vue content and adds our 3 (empty) pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,24 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    redirect: "/login",
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/login",
+    name: "Login",
+    component: () => import("@/views/Login.vue"),
+  },
+  {
+    path: "/login/reset-password",
+    name: "ResetPassword",
+    component: () => import("@/views/ResetPassword.vue"),
+  },
+  {
+    path: "/signup",
+    name: "Signup",
+    component: () => import("@/views/Signup.vue"),
   },
 ];
 
