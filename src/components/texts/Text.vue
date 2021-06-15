f1148e638291d5537756e34c23dd3034425f7ec8
JyToSTT <jean.tostivint@gmail.com>
2021-06-15 16:32:10 +0200

    > Added texts components

---
+++ src/components/texts/Text.vue
@@ -0,0 +1,13 @@
+<template>
+  <p class="text">
+    <slot></slot>
+  </p>
+</template>
+
+<style lang="scss">
+@import "../../assets/scss/utils/vars/typography";
+
+.subheading {
+  @include text-body;
+}
+</style>
